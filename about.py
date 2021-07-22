370b2509085350b441fd3674137ae499845561b6
gcolson11 <73450215+gcolson11@users.noreply.github.com>
2021-07-21 21:42:24 -0400

    new folder

---
+++ about.py
@@ -0,0 +1,10 @@
+# about section
+
+import streamlit as st
+def app():
+    st.title('About')
+    st.write("Welcome to Read-y Books, a book recommendation engine designed to help you find the perfect book to read. \
+              The interface allows you to find books that you've read and give them a rating. Once complete \
+              you'll be recommended 10 books that should be right up your alley. Don't believe me? Check it out for \
+              yourself, or select the random choice button to get a sense of how it all works before inputting your favorites.")
+    st.write("If you're interested in the back end details of this project check out my [GitHub](https://github.com/gcolson11/Capstone_Books)")
